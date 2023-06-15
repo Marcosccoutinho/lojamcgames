5c63ab968ce24de9301c4b8e9d615fda85aa7c2d
Your Name <https://github.com/Marcosccoutinho>
2023-06-15 19:04:05 -0300

    adiçao de novos arquivos

--- script.js
+++ script.js
@@ -3,31 +3,31 @@
     {
       id:0,
       nome:'playstation 4 ',
-      img: 'image.jpg',
+      img: 'ps4-slim-500g.jpeg',
       quantidade:0
     },
     {
       id:1,
       nome:'xbox one ',
-      img: 'image.jpg',
+      img: 'xbox-one-fat-500g.jpeg',
       quantidade:0
     },
     {
       id:2,
       nome:'xbox one s ',
-      img: 'image.jpg',
+      img: 'xbox-one-s.jpeg',
       quantidade:0
     },
     {
       id:3,
       nome:'nintendo',
-      img: 'image.jpg',
+      img: 'nintendo-switch.jpeg',
       quantidade:0
     },
     {
       id:4,
       nome:'kit teclado mouse e fone',
-      img: 'image.jpg',
+      img: 'kit-teclado-mouse-fone.jpeg',
       quantidade:0
     },
   ]
@@ -35,7 +35,28 @@
   inicializarLoja = () => {
     var containerProdutos = document.getElementById('produtos');
     items.map((val)=> {
-      console.log(val.nome); //Este código em JavaScript está usando o método map 
+      containerProdutos.innerHTML += `
+      
+      <div class="produto-single">
+      <img src= "`+val.img+`"/>
+      <p>`+val.nome+`</p>
+      <a key=`+val.id+`"href="">Adicionar ao carrinho!</a>
+
+
+
+
+
+
+
+
+
+      </div>
+      
+      
+      
+      
+      
+                                `//Este código em JavaScript está usando o método map 
                              //uma chamada de matriz items. O método mapé usado para percorrer cada elemento
                             // de uma matriz e retornar uma nova matriz com os resultados da função de retorno
                            // de chamada aplicada a cada elemento.
